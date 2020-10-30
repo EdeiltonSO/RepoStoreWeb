25feb6a7325bf2b2a02f79473480ca58a936606a
Edeilton Oliveira <edeiltonso@gmail.com>
2020-10-30 14:26:24 -0300

    remove repositories

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const repo = document.getElementById(id);
+    repo.parentNode.removeChild(repo);
   }
 
   return (
